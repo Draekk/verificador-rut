fa322d04ca41303c3b5ea5ae543788f862765995
Draekk <rodriver1992@gmail.com>
2023-08-17 23:54:42 -0400

    creacion de consola de errores y su funcionalidad

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,20 @@
-const rutInput = document.querySelector('main input');
+const rutInput = document.querySelector('input');
 const button = document.querySelector('button');
 const outputContainer = document.querySelector('.c-h3');
 const rutOutput = document.querySelector('.c-h3 h3');
+const errorMsg = document.querySelector('.error');
+
 
 button.addEventListener('click', () => {
-    if(outputContainer.classList.contains('inactive')){
+    if(isNaN(Number(rutInput.value)) || rutInput.value.length != 8){
+        errorMsg.style.opacity = 100;
+        errorMsg.innerText = 'Introduzca un Rut válido';
+    } else if(rutInput.value.indexOf('.') !== -1){
+        errorMsg.style.opacity = 100;
+        errorMsg.innerText = 'Introduzca su Rut sin puntos';
+    } else if(outputContainer.classList.contains('inactive')){
         outputContainer.classList.toggle('inactive');
         button.style.opacity = 0;
+        errorMsg.style.opacity = 0;
     }
-})
+});
