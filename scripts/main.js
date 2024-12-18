2e3791be0daa8ad7f64b37554fc7346516e42f34
Draekk <rodriver1992@gmail.com>
2024-12-18 11:58:12 -0300

    ajustes menores en los estilos | se agregó funcion de reset a la aplicacion

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,57 @@
-const rutInput = document.querySelector('input');
-const button = document.querySelector('button');
-const outputContainer = document.querySelector('.c-h3');
-const rutOutput = document.querySelector('.c-h3 h3');
-const errorMsg = document.querySelector('.error');
+const rutInput = document.querySelector("input");
+const button = document.querySelector("button");
+const outputContainer = document.querySelector(".c-h3");
+const rutOutput = document.querySelector(".c-h3 h3");
+const errorMsg = document.querySelector(".error");
 
+button.addEventListener("click", () => {
+  if (isNaN(Number(rutInput.value)) || rutInput.value.length != 8) {
+    errorMsg.style.opacity = 100;
+    errorMsg.innerText = "Introduzca un Rut válido";
+  } else if (rutInput.value.indexOf(".") !== -1) {
+    errorMsg.style.opacity = 100;
+    errorMsg.innerText = "Introduzca su Rut sin puntos";
+  } else if (outputContainer.classList.contains("inactive")) {
+    let rutDigit = returnDigit(rutInput.value);
+    rutOutput.innerText = `${Number(rutInput.value).toLocaleString(
+      "es-CL"
+    )}-${rutDigit}`;
 
-button.addEventListener('click', () => {
-    if(isNaN(Number(rutInput.value)) || rutInput.value.length != 8){
-        errorMsg.style.opacity = 100;
-        errorMsg.innerText = 'Introduzca un Rut válido';
-    } else if(rutInput.value.indexOf('.') !== -1){
-        errorMsg.style.opacity = 100;
-        errorMsg.innerText = 'Introduzca su Rut sin puntos';
-    } else if(outputContainer.classList.contains('inactive')){
-        let rutDigit = returnDigit(rutInput.value);
-        rutOutput.innerText = `${Number(rutInput.value).toLocaleString('es-CL')}-${rutDigit}`;
-
-        outputContainer.classList.toggle('inactive');
-        button.style.opacity = 0;
-        errorMsg.style.opacity = 0;
-    }
+    outputContainer.classList.toggle("inactive");
+    button.classList.add("inactive");
+    errorMsg.style.opacity = 0;
+  }
 });
 
+outputContainer.addEventListener("click", resetApp);
+
+function resetApp() {
+  rutInput.value = "";
+  button.classList.toggle("inactive");
+  outputContainer.classList.toggle("inactive");
+}
+
 function returnDigit(rut) {
-    let arrayRut = rut.split('');
-    let reverseRut = arrayRut.reverse();
-    let number = 2;
-    let total = 0;
-    let digit = 0;
-    for (const d of reverseRut) {
-        if(number > 7){
-            number = 2;
-        }
-        total = total + (Number(d) * number);
-        number++;
+  let arrayRut = rut.split("");
+  let reverseRut = arrayRut.reverse();
+  let number = 2;
+  let total = 0;
+  let digit = 0;
+  for (const d of reverseRut) {
+    if (number > 7) {
+      number = 2;
     }
-    digit = 11 - total%11;
-    
-    switch(digit){
-        case 10:
-            return 'K';
-        case 11:
-            return 0;
-        default:
-            return digit;
-    }
+    total = total + Number(d) * number;
+    number++;
+  }
+  digit = 11 - (total % 11);
+
+  switch (digit) {
+    case 10:
+      return "K";
+    case 11:
+      return 0;
+    default:
+      return digit;
+  }
 }
